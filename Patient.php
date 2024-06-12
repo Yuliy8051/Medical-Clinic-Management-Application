64f087bc28bfc50232631341d875d1a7e272c039
“Denys <denn8051@gmail.com>
2024-06-12 17:59:28 +0200

    Adding the attribute 'push' to the Patient class, adding it to the __construct function. Creation of insert_patient methed in the Patient class.

--- Patient.php
+++ Patient.php
@@ -5,13 +5,19 @@ class Patient
     private string $surname;
     private string $email;
     private string $password;
-    public function __construct(string $name, string $surname, string $email, string $password)
+    private bool $push;
+    public function __construct(string $name, string $surname, string $email, string $password, bool $push)
     {
         $this->name = $name;
         $this->surname = $surname;
         $this->email = $email;
         $this->password = $password;
+        $this->push = $push;
     }
-
+    public function insert_patient($database)
+    {
+        $query = "insert into users (first_name, last_name, email, password, push, role_ID) values ('$this->name', '$this->surname', '$this->email', '$this->password', '$this->push', 1);";
+        $database->query($query);
+    }
 }
 ?>
