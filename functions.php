7821e8c8a83a125899a6541d24a64af6ad6920a4
“Denys <denn8051@gmail.com>
2024-06-12 17:54:42 +0200

    Creation of the 'functions.php' file and adding functions for conneting to the database and creation of all the tables, triggers and intertion into dictionaries

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function connect_to_database(): PDO
+{
+    return new PDO('mysql:host=localhost;dbname=medical_clinic_management_application', 'root', '');
+}
+function create_database($database)
+{
+    $database->beginTransaction();
+    try {
+        $query = file_get_contents('create.sql');
+        $database->query($query);
+    }catch (Exception $exception){
+        $database->rollBack();
+        echo 'bad';
+    }
+}
+
+
