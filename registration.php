e97f86ac8db5b7e5a1b51da2cc218a9238e91a0c
“Denys <denn8051@gmail.com>
2024-06-12 18:09:11 +0200

    Adding functionality for hashing passwords and intertion patients into the database.

--- registration.php
+++ registration.php
@@ -1,12 +1,22 @@
 <?php
+require_once ('functions.php');
 require_once "Patient.php";
+$database = connect_to_database();
+if ($database->errorCode())
+    die('end');
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $pattern = "/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?:{}|<>])[A-Za-z\d!@#$%^&*(),.?:{}|<>]{6,}$/";
+if (isset($_POST['push']))
+    $push = true;
+else
+    $push = false;
 if (preg_match($pattern, $password)){
-    $patient = new Patient($name, $surname, $email, $password);
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $patient = new Patient($name, $surname, $email, $password, $push);
+    $patient->insert_patient($database);
     echo "good";
 }
 else {
