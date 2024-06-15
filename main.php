66ce078d3ee2c6a3dd34afc0750efe8016791d92
“Denys <denn8051@gmail.com>
2024-06-15 13:46:04 +0200

    Buttons to access to the administration panal, the doctor page and the page with cardiology visits.

--- main.php
+++ main.php
@@ -1,3 +1,10 @@
 <?php
-echo 'success';
+require_once 'Classes/User.php';
+session_start();
 echo '<a href="logout.php"><button>Log out</button></a>';
+if ($_SESSION['user']->getRoleID() == 2)
+    echo '<a href="administration.php"><button>Administration panel</button></a>';
+if ($_SESSION['user']->getRoleID() == 3)
+    echo '<a href="doctor.php"><button>Doctor panel</button></a>';
+if ($_SESSION['user']->getRoleID() == 1)
+    echo '<a href="cardiology_visits.php"><button>My cardiology visits</button></a>';
