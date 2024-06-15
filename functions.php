2bc324617b3714f8a420d16e51ba579e51872f6a
“Denys <denn8051@gmail.com>
2024-06-15 21:49:12 +0200

    The create_database() function was modified.

--- functions.php
+++ functions.php
@@ -3,11 +3,13 @@ function connect_to_database(): PDO
 {
     return new PDO('mysql:host=localhost;dbname=medical_clinic_management_application', 'root', '');
 }
-function create_database($database): void
+function create_database(PDO $database): void
 {
     $database->beginTransaction();
     try {
         $query = file_get_contents('create.sql');
+        $database->query($query);
+        $query = file_get_contents('insertProcedures.sql');
         $database->query($query);
     }catch (Exception $exception){
         $database->rollBack();
