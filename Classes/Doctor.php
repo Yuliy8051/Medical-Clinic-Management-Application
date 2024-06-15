26b348f20557873747d21f882540e48aacab625c
“Denys <denn8051@gmail.com>
2024-06-15 13:27:30 +0200

    The constructor was modified. New getter for the medical_specialisation_ID attribute

--- Classes/Doctor.php
+++ Classes/Doctor.php
@@ -1,10 +1,14 @@
 <?php
 class Doctor extends Assistant
 {
-    public function __construct(string $first_name, string $last_name, string $email, string $password, bool $push, int $role_ID, int $medical_specialisation_ID)
+    public function __construct($ID, string $first_name, string $last_name, string $email, string $password, bool $push, int $role_ID, int $medical_specialisation_ID)
     {
-        parent::__construct($first_name, $last_name, $email, $password, $push, $role_ID);
+        parent::__construct($ID, $first_name, $last_name, $email, $password, $push, $role_ID);
         $this->medical_specialisation_ID = $medical_specialisation_ID;
     }
 
+    public function getMedicalSpecialisationID(): int
+    {
+        return $this->medical_specialisation_ID;
+    }
 }
