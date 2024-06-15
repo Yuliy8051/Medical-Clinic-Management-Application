2805bf5cf3e4a5b6f0e5d13eb14f565122b40d39
“Denys <denn8051@gmail.com>
2024-06-15 13:42:38 +0200

    A page used for watching a patient's cargiology visits.

---
+++ cardiology_visits.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My cardiology visits</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td>Date and time of adding</td>
+        <td>Meeting Description</td>
+        <td>Recommendations</td>
+        <td>Cardiologist</td>
+    </tr>
+<?php
+require_once 'functions.php';
+require_once 'Classes/User.php';
+session_start();
+$database = connect_to_database();
+$_SESSION['user']->printVisits($database);
+?>
+</table>
+</body>
+</html>
