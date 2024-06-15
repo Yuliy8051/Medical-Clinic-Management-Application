2fe22209101a56067f359c9ad52e7bf40b3125be
“Denys <denn8051@gmail.com>
2024-06-15 13:20:00 +0200

    The constructor was modified. New methods for printing medical specailisations and inserting a new doctor into the database.

--- Classes/Admin.php
+++ Classes/Admin.php
@@ -1,9 +1,45 @@
 <?php
 class Admin extends Cardiologist
 {
-    public function __construct(string $first_name, string $last_name, string $email, string $password, bool $push, int $role_ID)
+    public function __construct($ID, string $first_name, string $last_name, string $email, string $password, bool $push, int $role_ID)
     {
-        parent::__construct($first_name, $last_name, $email, $password, $push, $role_ID);
+        parent::__construct($ID, $first_name, $last_name, $email, $password, $push, $role_ID);
     }
-
+    public function printMedicalSpecialisations(PDO $database):void
+    {
+        $query = "select * from medical_specialisations";
+        $query_result = $database->query($query);
+        while ($result = $query_result->fetch(PDO::FETCH_ASSOC)) {
+            printf('<option value="%d">%s</option>', $result['ID'], $result['medical_specialisation']);
+        }
+    }
+     public function check_insert($database): void
+     {
+        if (isset($_POST['add_doctor'])) {
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            if (isset($_POST['push']))
+                $push = 1;
+            else
+                $push = 0;
+            $img = addslashes(file_get_contents($_FILES['photo']['tmp_name']));
+            $img_type = substr($_FILES['photo']['type'], 0, 5);
+            if ($img_type === 'image'){
+                $query = "insert into users (first_name, last_name, email, password, push, role_ID) values ('{$_POST['first_name']}', '{$_POST['last_name']}', '{$_POST['email']}', '$password', $push, 3);
+insert into employees (ID, biography, qualification, photo, medical_specialisation_ID) values ((select max(ID) from users), '{$_POST['biography']}', '{$_POST['qualification']}', '$img', {$_POST['medical_specialisation_ID']});";
+                $this->insert($database, $query);
+            }
+            else{
+                echo 'The file must be image';
+            }
+        }
+     }
+     public function insert($database, $query): void
+     {
+         try {
+            $database->query($query);
+            header("Location:administration.php");
+         } catch (Exception $exception) {
+            echo $exception->getMessage();
+         }
+     }
 }
