e249d2c744d7bcb67f06ea290e88c56b371d1f0f
“Denys <denn8051@gmail.com>
2024-06-21 15:42:01 +0200

    Functionality for making an appointment with a doctor. Files for registration, logging in and out are in one folder.

---
+++ log/registration.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../functions.php';
+require_once "../Classes/User.php";
+$database = connect_to_database();
+if ($database->errorCode())
+    die('end');
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = strtolower($_POST['email']);
+$password = $_POST['password'];
+$pattern = "/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?:{}|<>])[A-Za-z\d!@#$%^&*(),.?:{}|<>]{6,}$/";
+if (isset($_POST['push']))
+    $push = true;
+else
+    $push = false;
+if (preg_match($pattern, $password)){
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $patient = new Patient(0, $name, $surname, $email, $password, $push);
+    $patient->insertPatient($database);
+    header('Location:../index.php');
+}
+else {
+    require_once "registration.html";
+    echo "The password should consist of at least 6 characters and contain at least 1 uppercase letter, a number and a special character";
+}
+?>
